b4ecd504474e25a82f734a3daff0779f68f24352
Ricardo <rh2512@imperial.ac.uk>
2016-02-08 07:54:09 +0000

    Runs sim for 3 times to get avg energy cost

--- simultationGenerator.py
+++ simultationGenerator.py
@@ -1,31 +1,39 @@
 import itertools
 import run
-
+import sys
+import math
 """
 listOfFlags=['-fetch:ifqsize',  '-ruu:size', '-lsq:size', '-mem:width', '-res:ialu', '-res:imult', '-res:fpalu', '-res:fpmult', '-issue:inorder', '-issue:wrongpath', '-res:memports', '-fetch:mplat', '-issue:width','-bpred']
 """
 
-limit = 2
-listOfFlags=[('-fetch:ifqsize', [x for x in range(1, 8)]),
-		('-ruu:size', [x**2 for x in range(2, 8)]),
-		('-lsq:size', [x for x in range(1, 8)]), 
-		('-mem:width',[x*32 for x in range(1, 3)]),
-		('-res:ialu', [x for x in range(1, 8)]),
-		('-res:imult',[x for x in range(1, 8)]),
-		('-res:fpalu',[x for x in range(1, 8)]),
-		('-res:fpmult',[x for x in range(1, 8)])
+limit = 7
+listOfFlags=[('-fetch:ifqsize', [x for x in range(1, limit)]),
+		('-ruu:size', [int(math.pow(2, x)) for x in range(2, limit)]),
+		('-lsq:size', [x for x in range(1, limit)]), 
+		('-res:ialu', [x for x in range(1, limit)]),
+		('-res:imult',[x for x in range(1, limit)]),
+		('-res:fpalu',[x for x in range(1, limit)]),
+		('-res:fpmult',[x for x in range(1, limit)])
 		]
 
 #Performs an exhaustive search with all possible parameters of simple scalar
 def runSim():
 	unzipped = map(list, zip(*listOfFlags))
-	for values in itertools.product(*unzipped[1]):
+	setOfParams = set(itertools.product(*unzipped[1]))
+	#print list(setOfParams)[0:100]
+	#sys.exit(0)
+	count = 0
+	for values in list(setOfParams):
 		commandString = ""
-		for i in range(len(listOfFlags)):
-			commandString = commandString + " " + listOfFlags[i][0] + " " + str(values[i])
-			print "Running: ", commandString
+		for i in range(len(unzipped[0])):
+				flag = unzipped[0][i]
+				valueOfFlag = str(values[i])
+				commandString = commandString + " " + flag + " " + valueOfFlag
+		#Run three simulations for statistical significance
+		for k in range(1, 4):
+			print "Running(%d): "%(k), commandString
 			run.main(commandString, 7)	
-
+	print count
 if __name__ == "__main__":
 	runSim()
 	"""
